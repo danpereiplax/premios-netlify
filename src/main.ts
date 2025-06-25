c8a6461cc1bf80290becffd647a1ce59d2099eeb
danperec <dpereirac@alumno.iplacex.cl>
2025-06-25 17:19:39 -0400

    PWA

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [
+    ...appConfig.providers,
+    importProvidersFrom(
+      environment.production
+        ? ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: true,
+            registrationStrategy: 'registerWhenStable:30000'
+          })
+        : []
+    )
+  ]
 }).catch(err => console.error(err));
